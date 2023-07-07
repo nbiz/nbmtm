1e1894a05fc9c4f6033cdb12c4bbec64000af1d7
nbiz <nbisnaire@gmail.com>
2023-07-07 18:21:19 -0400

    truck emoji test

--- assets/script.js
+++ assets/script.js
@@ -38,3 +38,93 @@ $('#lead-form').on('submit', function (event) {
         }
     });
 });
+
+//silly truck emojis
+let intervalId = null;
+let isMouseDown = false;
+let mouseX, mouseY;
+
+document.addEventListener('mousemove', function (e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+});
+
+document.addEventListener('mousedown', function (e) {
+    isMouseDown = true;
+    intervalId = setInterval(() => {
+        if (isMouseDown) {
+            for (let i = 0; i < 5; i++) {
+                createEmoji(mouseX, mouseY);
+            }
+        }
+    }, 50);
+});
+
+
+document.addEventListener('mouseup', function (e) {
+    isMouseDown = false;
+    if (intervalId) {
+        clearInterval(intervalId);
+        intervalId = null;
+    }
+});
+
+// Get all dropdowns
+const dropdowns = document.querySelectorAll('select');
+
+// Stop the emoji generation when a dropdown is opened
+dropdowns.forEach(dropdown => {
+    dropdown.addEventListener('focus', function () {
+        isMouseDown = false;
+        if (intervalId) {
+            clearInterval(intervalId);
+            intervalId = null;
+        }
+    });
+
+    // Resume the emoji generation when a dropdown is closed
+    dropdown.addEventListener('blur', function () {
+        isMouseDown = false;
+    });
+});
+
+
+function createEmoji(x, y) {
+    const emoji = document.createElement('div');
+    emoji.className = 'emoji';
+    emoji.innerText = '🚚';
+    emoji.style.left = `${x}px`;
+    emoji.style.top = `${y}px`;
+
+    const xRatio = (Math.random() - 0.5) * 200;
+    const yRatio = -(Math.random() * 0.5 + 2.5) * 200;
+    const rotateRatio = (Math.random() * 2 - 1) * 25;
+
+    const animationDuration = Math.random() * 1 + 1.5;
+    const totalFrames = Math.floor(animationDuration * 60);
+
+    let frameCount = 0;
+
+    function updatePosition() {
+        const t = frameCount / totalFrames;
+        const easedT = t < 0.5 ? 2 * t * t : 1 - Math.pow(-2 * t + 2, 2) / 2;
+
+        const x = xRatio * easedT;
+
+        const y = yRatio * easedT + 0.5 * 9.8 * Math.pow(easedT * animationDuration, 2) * 100;
+
+        emoji.style.transform = `translate(${x}px, ${y}px) rotate(${rotateRatio}deg)`;
+
+        frameCount++;
+
+        if (frameCount < totalFrames) {
+            requestAnimationFrame(updatePosition);
+        } else {
+            document.body.removeChild(emoji);
+        }
+    }
+
+    updatePosition();
+
+    document.body.appendChild(emoji);
+}
